a9b1587316b396c39da10915207a686b6be1cb67
Peeter Piegaze <1153481+ppiegaze@users.noreply.github.com>
2024-05-27 14:36:13 +0200

    wip

--- build-simple.py
+++ build-simple.py
@@ -1,36 +1,11 @@
-SITEMAP = './sitemap.md'
+import json
+SITEMAP = './sitemap.json'
 
 
-def parse(lines, level=0) -> list:
-    tree: list = []
-    while lines:
-        line: str = lines[0]
-        line = line.rstrip(' \n')
-        current_level: float = (len(line) - len(line.lstrip())) / 4
-        if current_level.is_integer():
-            current_level: int = int(current_level)
-        else:
-            raise ValueError(f'Error in sitemap line "{line}": Indentations must be multiples of 4 spaces')
-        if current_level == level:
-            lines.pop(0)
-            node = [line.strip()]
-            children = parse(lines, level + 1)
-            if children:
-                node.extend(children)
-                tree.append(node)
-            else:
-                tree.append(node)
-        else:
-            break
-    return tree
+def build_sphinx_docs():
+    with open(SITEMAP, "r") as read_file:
+        tree = json.load(read_file)
+    print(tree)
 
 
-def do_parse(lines) -> list:
-    tree = parse(lines)
-    return tree[0]
-
-
-with open(SITEMAP, 'r') as file:
-    my_lines: list = file.readlines()
-my_tree: list = do_parse(my_lines)
-print(my_tree)
+build_sphinx_docs()
