e5b660b806889f8786d4f78d0b9ea874da4e2728
Peeter Piegaze <1153481+ppiegaze@users.noreply.github.com>
2024-05-28 16:37:37 +0200

    wip

--- build.py
+++ build.py
@@ -5,12 +5,12 @@ import shlex
 import json
 
 
-SOURCE_DIR = './source'
-TEMP_DIR = './temp'
-BUILD_DIR = './build/html'
-SITEMAP = './sitemap.json'
-ALL_TAGS = ['serverless', 'byoc']
-SUBS = {
+SOURCE_DIR: str = './source'
+SPHINX_SOURCE_DIR: str = './sphinx_source'
+BUILD_DIR: str = './build/html'
+SITEMAP: str = './sitemap.json'
+ALL_TAGS: list[str] = ['serverless', 'byoc']
+SUBS: dict[str: dict[str: str] | str: str] = {
     'product_name': {
         'byoc': 'Union BYOC',
         'serverless': 'Union Serverless',
@@ -21,15 +21,15 @@ SUBS = {
 
 # Call a shell command.
 # Supports interpolation of global variables.
-def shell(command):
+def shell(command: str) -> None:
     subprocess.run(shlex.split(command))
 
 
 # Process a single unionai-docs Markdown file.
 # A unionai-docs file is a Sphinx/Myst Markdown file augmented
 # with Jinja2 templating but without any `toctree` directives.
-def process_md_file(input_path, output_path, var_list, toctree=None):
-    env = jinja2.Environment(
+def create_sphinx_file(path: str, variant: str, tags: list[str], toctree: str = None) -> None:
+    env: jinja2.Environment = jinja2.Environment(
         loader=jinja2.FileSystemLoader(os.getcwd()),
         block_start_string='{@@',
         block_end_string='@@}',
@@ -40,9 +40,14 @@ def process_md_file(input_path, output_path, var_list, toctree=None):
         trim_blocks=True,
         lstrip_blocks=True,
     )
-    template = env.get_template(input_path)
-    output = template.render(var_list)
-    append_toctree(output, toctree)
+    input_path: str = f'{SOURCE_DIR}/{path}'
+    output_path: str = f'{SPHINX_SOURCE_DIR}/{variant}/{path}'
+    template: jinja2.Template = env.get_template(input_path)
+    output:str = template.render(get_vars(variant))
+
+    # TODO: figure out how to add the tags to the top of the output
+    output = tags + output + toctree
+    
     with open(output_path, 'w') as f:
         f.write(output)
 
@@ -50,64 +55,67 @@ def process_md_file(input_path, output_path, var_list, toctree=None):
 # Returns a dictionary of variables based on the global SUBS dictionary,
 # filtered for the specified variant and augmented with the variant itself
 # as a boolean variable set to True.
-def get_var_list(variant) -> dict:
-    var_list = {}
+def get_vars(variant: str) -> dict:
+    vl = {}
     for key, value in SUBS.items():
         if isinstance(value, dict):
-            var_list[key] = value[variant]
+            vl[key] = value[variant]
         elif isinstance(value, str):
-            var_list[key] = value
-    var_list[variant] = True
-    return var_list
+            vl[key] = value
+    vl[variant] = True
+    return vl
 
 
-def process_page(page, parent_path=None, parent_tags=None):
-    name = page['name']
-    tags = page['tags']
-    children = page['children']
-    path = os.path.join(parent_path, name).rstrip(' /')
+def process_page_node(page_node: dict, variant: str, path: str = None, parent_tags: str = None) -> str:
+    name = page_node['name']
+    title = page_node['title']
+    tags = page_node['tags']
+    children = page_node['children']
+    path = os.path.join(path, name).rstrip(' /')
 
-    # If tags of current page include element not present in parent page tags raise error.
-    # A page for a variant cannot exist unless its parent page also exists for that variant.
-    if not set(tags) > set(parent_tags):
+    # If tree is malformed, exit.
+    if set(tags) > set(parent_tags):
         raise ValueError(f'Error processing {path}: tags of current page include element not present in parent page tags. A page for a variant cannot exist unless its parent page also exists for that variant.')
 
-    # If the page has no children then its file location is {path}.md and it has no toctree
-    if not children:
-        for tag in tags:
-            var_list = get_var_list(tag)
-            process_md_file(
-                f'{SOURCE_DIR}/{path}.md',
-                f'{TEMP_DIR}/{tag}/{path}.md',
-                var_list
-            )
+    # If this page does not appear in the current variant site return `None`.
+    if variant not in tags:
+        return None
 
-    # If the page has children then its file location is {parent_path}/{name}/index.md
+    # If this page has no children:
+    # The file path for this page `{path}.md`.
+    # Create the Sphinx file for this page, processed for the current variant.
+    # Do not add a toctree to this page.
+    # Return the toctree entry of this page in its parent page: `{name}`.
+    if not children:
+        create_sphinx_file(f'{path}.md', variant, tags)
+        return name
+
+    # This page does have children:
+    # Call `process_page_node` on each child, assembling the returned toctree entries into a toctree
+    # The file path for this page is `{path}/index.md`.
+    # Create the Sphinx file for this page, processed for the current variant.
+    # Add the assembled toctree to this page.
+    # Return the toctree entry of this page in its parent page: `{name}/index`
     else:
-        for child in children:
-            
-        var_list = get_var_list(tag)
-        process_md_file(
-            f'{SOURCE_DIR}/{path}/index.md',
-            f'{TEMP_DIR}/{tag}/{path}.md',
-            var_list,
-            toctree
-        )
+        toctree: str = '```{toctree}\n:maxdepth: 2\n:hidden:\n\n'
+        for child_page_node in children:
+            toctree += process_page_node(child_page_node, path, tags, variant) + '\n'
+        toctree += '```\n'
+        create_sphinx_file(f'{path}/index.md', variant, tags, toctree)
+        return name + '/index'
 
 
 def process_project():
     shell(f'rm -rf {BUILD_DIR}')
-    shell(f'rm -rf {TEMP_DIR}')
+    shell(f'rm -rf {SPHINX_SOURCE_DIR}')
     with open(SITEMAP, "r") as sm:
-        page = json.load(sm)
-    process_page(page)
+        page_node = json.load(sm)
     for tag in ALL_TAGS:
-        shell_command(f'sphinx-build {TEMP_DIR}/{tag} {BUILD_DIR}/{tag}')
-    shell_command(f'cp ./index.html {BUILD_DIR}/index.html')
+        process_page_node(page_node, tag)
+    for tag in ALL_TAGS:
+        shell(f'sphinx-build {TEMP_DIR}/{tag} {BUILD_DIR}/{tag}')
+    shell(f'cp ./index.html {BUILD_DIR}/index.html')
 
 
 if __name__ == "__main__":
-    with open(SITEMAP, 'r') as file:
-        lines = file.readlines()
-    tree = get_tree(lines, 0)
-    print(tree)
+    process_project()
