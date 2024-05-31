d174c202860bb3bc922b0a9af466cd673073a1ce
Peeter Piegaze <1153481+ppiegaze@users.noreply.github.com>
2024-05-31 14:35:15 +0200

    add drop down

--- build.py
+++ build.py
@@ -69,9 +69,8 @@ def create_sphinx_file(path: str, variant: str, tags: list[str], toctree: str = 
         print(f'{indent}File not found at {input_path}')
     else:
         output: str = template.render(get_vars(variant)).strip()
-        frontmatter = f'---\n variants: {str(tags)}\n---\n\n'
-        tags_directive = '```{tags} ' + variant + '\n```\n\n'
-        output = frontmatter + tags_directive + output + toctree
+        frontmatter = f'---\nall-variants: {str(tags)}\nthis-variant: {variant}\n---\n\n'
+        output = frontmatter + output + toctree
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
         with open(output_path, 'w') as f:
             f.write(output)
--- source/conf.py
+++ source/conf.py
@@ -8,8 +8,9 @@ release = '1.0'
 master_doc = 'index'
 html_static_path = ['_static']
 templates_path = ['_templates']
+html_css_files = ['custom.css']
 exclude_patterns = []
-extensions = ['myst_parser', 'sphinx_design', 'sphinx_reredirects', 'sphinx_tags']
+extensions = ['myst_parser', 'sphinx_design', 'sphinx_reredirects']
 html_theme = 'sphinx_book_theme'
 
 # Myst
@@ -18,6 +19,10 @@ myst_heading_anchors = 6
 
 # Sphinx book theme
 html_logo = '_static/public/logo.svg'
+
+html_sidebars = {
+    "**": ['navbar-logo.html', 'variant-selector.html', 'search-button-field.html', 'sbt-sidebar-nav.html']
+}
 
 # Redirects
 redirects = {
