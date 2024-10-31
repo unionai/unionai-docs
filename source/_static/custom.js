068e1ae8ec5847d0a50ffc96ac169c1fbad246c9
chmod77 <chmod777.ke@gmail.com>
2024-10-31 14:47:22 +0300

    feat: move path checker from JS

--- source/_static/custom.js
+++ source/_static/custom.js
@@ -8,22 +8,4 @@ window.addEventListener("DOMContentLoaded", function() {
         link.setAttribute("target", "_blank");
     });
 
-    // hide login/signup buttons on BYOC pages
-    if (window.location.pathname.includes('/byoc/')) {
-        // const loginButton = document.querySelector('.unionai-btn-login');
-        const signupButton = document.querySelector('.unionai-btn-signup');
-        
-        // if (loginButton) {
-        //     loginButton.parentElement.style.display = 'none';
-        // }
-        if (signupButton) {
-            signupButton.parentElement.style.display = 'none';
-        }
-        
-        //  hide vertical separator if it's the last one
-        const separators = document.querySelectorAll('.vertical-separator');
-        if (separators.length > 0) {
-            separators[separators.length - 1].style.display = 'none';
-        }
-    }
 });
