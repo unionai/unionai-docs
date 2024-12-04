2b4cf4c7b389c4a3dc2eef2bdb40158b7a1e82fa
chmod77 <chmod777.ke@gmail.com>
2024-12-04 22:49:39 +0300

    chore: remove the -j auto flag to ensure serial writes (sphinxext cries foul on parallelized writes

--- build.py
+++ build.py
@@ -362,7 +362,7 @@ def process_project():
         shell(f'cp -r {SOURCE_DIR}/_templates {SPHINX_SOURCE_DIR}/{variant}')
     for variant in ALL_VARIANTS:
         shell(
-            f'sphinx-build -j auto {SPHINX_SOURCE_DIR}/{variant} {BUILD_DIR}/{variant}',
+            f'sphinx-build {SPHINX_SOURCE_DIR}/{variant} {BUILD_DIR}/{variant}',
             env=DOCSEARCH_CREDENTIALS[variant],
         )
     shell(f'cp ./_redirects {BUILD_DIR}/_redirects')
