6e5c28ffeb8e143b0e090da2c813a66666abdaba
Nelson Araujo <nelson@lunenetworks.com>
2025-03-07 15:37:03 -0800

    Less dramatic red message.

--- scripts/dev-pre-flight.sh
+++ scripts/dev-pre-flight.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 if [[ ! -e hugo.local.toml ]]; then
-  echo -e "\033[0;41m"
-  cat <<EOF
-
-+----------------------------------------------------------------------+
-| FATAL: You need to have a 'hugo.local.toml' for development mode.    |
-|        (please copy it from 'hugo.local.toml~sample' to get started) |
-+----------------------------------------------------------------------+
-
-EOF
-  echo -e "\033[0m"
+  echo
+  echo -e "\033[0;41m+----------------------------------------------------------------------+\033[0m" 
+  echo -e "\033[0;41m| FATAL: You need to have a 'hugo.local.toml' for development mode.    |\033[0m" 
+  echo -e "\033[0;41m|        (please copy it from 'hugo.local.toml~sample' to get started) |\033[0m" 
+  echo -e "\033[0;41m+----------------------------------------------------------------------+\033[0m" 
+  echo
   exit 1
 fi
