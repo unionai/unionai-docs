49f376d61498924f41c9e46ab4d7d1b9f603824f
Nelson Araujo <nelsonjr@users.noreply.github.com>
2025-03-15 01:29:50 -0700

    Ensure legacy settings are detected.

--- scripts/dev-pre-flight.sh
+++ scripts/dev-pre-flight.sh
@@ -9,3 +9,20 @@ if [[ ! -e hugo.local.toml ]]; then
   echo
   exit 1
 fi
+
+legacy=0
+if grep -q -c variant_hide_in_development "hugo.local.toml"; then
+  legacy=1
+fi
+if grep -q -c variant_identify_variant_in_development "hugo.local.toml"; then
+  legacy=1
+fi
+
+if [[ $legacy -eq 1 ]]; then
+  echo -e "\033[0;41m+----------------------------------------------------------------------+\033[0m"
+  echo -e "\033[0;41m| FATAL: You have legacy settings in 'hugo.local.toml'.                |\033[0m"
+  echo -e "\033[0;41m|        (please copy settings from 'hugo.local.toml~sample')          |\033[0m"
+  echo -e "\033[0;41m+----------------------------------------------------------------------+\033[0m"
+  echo
+  exit 1
+fi
