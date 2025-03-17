93919e4af6bbf15800cd4ff292c32647f41c5964
Nelson Araujo <nelsonjr@users.noreply.github.com>
2025-03-17 10:03:46 -0700

    Add verification of package.

--- tools/api_generator/main_parser.py
+++ tools/api_generator/main_parser.py
@@ -1,23 +1,30 @@
 import argparse
-from typing import Dict, List
-import yaml
-from parser.packages import get_subpackages
-from parser.classes import get_classes
-from ptypes import ParsedInfo
 from importlib import metadata
+from sys import stderr
+
+import yaml
+
+from parser.classes import get_classes
+from parser.packages import get_subpackages
+from ptypes import ParsedInfo
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Generate API documentation from Python package"
     )
-    parser.add_argument("--package", help="Package to parse")
+    parser.add_argument("--package", help="Package to parse", required=True)
     args = parser.parse_args()
 
-    pkgs = get_subpackages(args.package)
-
     root_package = args.package.split(".")[0]
-    version = metadata.version(root_package)
+
+    try:
+        version = metadata.version(root_package)
+    except metadata.PackageNotFoundError:
+        print(f"FATAL: Package {root_package} not found. Did you have it installed?", file=stderr)
+        exit(1)
+
+    pkgs = get_subpackages(args.package)
 
     clss = {}
     for pkg in pkgs:
